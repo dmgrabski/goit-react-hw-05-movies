a3ea280667991b0187abdf6f887a06dabff3e656
dmgrabski <dmgrabski97@gmail.com>
2024-02-05 19:11:26 +0100

    update3

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -2,32 +2,42 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { fetchMovieReviews } from "../../service/Api";
 import Loader from "../Loader/Loader";
+
 const Reviews = () => {
   const { movieID } = useParams();
   const [reviews, setReviews] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+
   useEffect(() => {
-    let subscribed = true;
+    let subscribed = true; // Kontroluje, czy komponent jest zamontowany
+
     const fetchData = async () => {
+      setIsLoading(true);
       try {
-        setIsLoading(true);
-        const movie = await fetchMovieReviews(movieID);
-        setReviews(movie);
-        setIsLoading(false);
-      } catch (error) {}
+        const movieReviews = await fetchMovieReviews(movieID);
+        if (subscribed) { // Aktualizuje stan tylko jeśli komponent jest zamontowany
+          setReviews(movieReviews);
+        }
+      } catch (error) {
+        console.error(error); // Logowanie błędów
+      } finally {
+        if (subscribed) {
+          setIsLoading(false); // Zawsze wyłącz ładowanie, jeśli komponent jest zamontowany
+        }
+      }
     };
 
     fetchData();
 
     return () => {
-      subscribed = false;
+      subscribed = false; // Zaznacz, że komponent został odmontowany
     };
   }, [movieID]);
 
   return (
     <>
       {isLoading && <Loader />}
-      {reviews && (
+      {reviews.length > 0 ? (
         <div>
           <h2>Movie Reviews</h2>
           <ul>
@@ -39,9 +49,11 @@ const Reviews = () => {
             ))}
           </ul>
         </div>
+      ) : (
+        <div>We don't have any reviews for this movie.</div>
       )}
-      {!reviews && <div>We don't have any reviews for this movie.</div>}
     </>
   );
 };
+
 export default Reviews;
