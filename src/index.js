636b29e386bd26e77711113bd9311b86ff31bded
dmgrabski <dmgrabski97@gmail.com>
2024-02-06 18:32:22 +0100

    update7

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom'; // Import BrowserRouter
+import { HashRouter as Router } from 'react-router-dom'; // Zmieniono na HashRouter
 import App from './components/App';
 import "./main.css";
 
